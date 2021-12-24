0e82502d7734ac2353946f711893e1d278b42c46
Murzin Vitaly <murzinvit@gmail.com>
2021-12-24 10:57:26 +0500

    Modify outputs.tf

--- yandex/outputs.tf
+++ yandex/outputs.tf
@@ -1,19 +1,19 @@
 # outputs
 
-output "internal_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+output "internal_ip_address_nat-1" {
+  value = yandex_compute_instance.nat-1.network_interface.0.ip_address
 }
 
-output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+output "external_ip_address_nat-1" {
+  value = yandex_compute_instance.nat-1.network_interface.0.nat_ip_address
 }
 
 output "internal_ip_address_vm_2" {
   value = yandex_compute_instance.vm-2.network_interface.0.ip_address
 }
 
-output "external_ip_address_vm_2" {
-  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+output "external_ip_address_vm_2 {
+  value = yandex_compute_instance.vm_2.network_interface.0.nat_ip_address
 }
 
 output "internal_ip_address_vm_3" {
