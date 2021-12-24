4f690d0306b1f9fb6047d3276a1096e65d79cfde
Murzin Vitaly <murzinvit@gmail.com>
2021-12-24 11:04:50 +0500

    Modify outputs.tf

--- yandex/outputs.tf
+++ yandex/outputs.tf
@@ -13,7 +13,7 @@ output "internal_ip_address_vm_2" {
 }
 
 output "external_ip_address_vm_2" {
-  value = yandex_compute_instance.vm_2.network_interface.0.nat_ip_address
+  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
 }
 
 output "internal_ip_address_station-2" {
