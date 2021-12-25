08e1abbe2f24e95074952f1499240f5a9f155a95
Murzin.V <murzinvit@gmail.com>
2021-12-25 07:38:10 -0500

    correct

--- yandex/bucket.tf
+++ yandex/bucket.tf
@@ -2,6 +2,7 @@ resource "yandex_storage_bucket" "netologybucket" {
   access_key = var.bucket_access_key
   secret_key = var.bucket_secret_key
   bucket     = "netologybucket"
+  force_destroy      = true
   acl        = "public-read"
   website {
     index_document = "index.html"
