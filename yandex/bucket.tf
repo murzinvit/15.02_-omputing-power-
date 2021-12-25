320c8788ef8053a66081ea8ebc094ce1fa33b841
Murzin.V <murzinvit@gmail.com>
2021-12-25 02:38:06 -0500

    modify bucket name correct

--- yandex/bucket.tf
+++ yandex/bucket.tf
@@ -1,17 +1,18 @@
-resource "yandex_storage_bucket" "ioakim" {
+resource "yandex_storage_bucket" "netology" {
   access_key = var.bucket_access_key
   secret_key = var.bucket_secret_key
-  bucket     = "ioakim"
+  bucket     = "netologybucket"
   acl        = "public-read"
   website {
-    index_document = "files/index.html"
+    index_document = "index.html"
   }
 }
 
 resource "yandex_storage_object" "index-object" {
   access_key = var.bucket_access_key
   secret_key = var.bucket_secret_key
-  bucket     = "ioakim"
+  acl        = "public-read"
+  bucket     = "netologybucket"
   key        = "index.html"
   source     = "files/index.html"
 }
@@ -19,7 +20,8 @@ resource "yandex_storage_object" "index-object" {
 resource "yandex_storage_object" "image-object" {
   access_key = var.bucket_access_key
   secret_key = var.bucket_secret_key
-  bucket     = "ioakim"
+  acl        = "public-read"
+  bucket     = "netologybucket"
   key        = "americano"
   source     = "files/americano.jpg"
 }
